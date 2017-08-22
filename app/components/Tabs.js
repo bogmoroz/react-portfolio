76dd9be47afa196726f9e73a791e1c1711343c07
Bogdan Moroz <boggy.frost@gmail.com>
2017-08-23 00:46:22 +0300

    Basic navigation bar

--- app/components/Tabs.js
+++ app/components/Tabs.js
@@ -1,9 +1,39 @@
 var React = require('react');
 
 class Tabs extends React.Component {
+
+  constructor (props) {
+    super(props);
+    this.state = {
+      selectedTab: 'Home'
+    };
+
+    this.changeTab = this.changeTab.bind(this);
+  }
+
+  changeTab(tab) {
+    this.setState(function() {
+      return {
+        selectedTab: tab
+      }
+    });
+  }
+
   render() {
+    var tabNames = ['Home', 'Software', 'Media', 'Contacts'];
     return (
-      <div>Multiple tabs</div>
+      <ul className='tab-menu'>
+        {tabNames.map(function (tab) {
+          return(
+            <li
+              style={tab === this.state.selectedTab ? { color: '#f79c13' } : null}
+              onClick={this.changeTab.bind(null, tab)}
+              key={tab}>
+              {tab}
+            </li>
+          )
+        }, this)}
+      </ul>
     )
   }
 }
