a179a02af11ac9085c002e09e034091cb7afbc27
Bogdan Moroz <boggy.frost@gmail.com>
2019-09-02 01:11:06 +0300

    MenuButton extracted into a separate component

--- app/components/Landing.js
+++ app/components/Landing.js
@@ -3,17 +3,14 @@ import style from './styles/main.scss';
 import Nav from './Nav';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './Home';
+import MenuButton from './MenuButton';
 
 export default class Landing extends React.Component {
   render() {
     return (
       <Router>
         <header>
-          <div className='menu-btn'>
-            <div className='btn-line'></div>
-            <div className='btn-line'></div>
-            <div className='btn-line'></div>
-          </div>
+          <MenuButton />
         </header>
 
         <Nav />
