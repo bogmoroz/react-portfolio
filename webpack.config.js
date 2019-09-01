9d13ee2d017fe871bb2ec7b65853eddd57f5dd67
Bogdan Moroz <boggy.frost@gmail.com>
2019-09-02 00:51:14 +0300

    Url loader and image background for body

--- webpack.config.js
+++ webpack.config.js
@@ -17,8 +17,11 @@ module.exports = {
         use: ['style-loader', 'css-loader', 'sass-loader']
       },
       {
-        test: /\.(png|woff|woff2|eot|ttf|svg)$/,
-        loader: 'url-loader?limit=100000'
+        test: /\.(jpg|png|woff|woff2|eot|ttf|svg)$/,
+        loader: 'url-loader',
+        options: {
+          limit: 1000000
+        }
       }
     ]
   },
