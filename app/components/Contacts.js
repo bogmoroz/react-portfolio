25572fd08f5deb8c458191f0ed61cc37beb3f411
Bogdan Moroz <boggy.frost@gmail.com>
2019-09-02 23:38:38 +0300

    Styled the contacts page

--- app/components/Contacts.js
+++ app/components/Contacts.js
@@ -4,16 +4,32 @@ export default function Contacts() {
   return (
     <>
       <div className='section-container contacts'>
-        <p>mr.bogdan.moroz@gmail.com</p>
-        <a
-          href='https://www.linkedin.com/in/bogdan-moroz-20b892113?trk=nav_responsive_tab_profile_pic'
-          target='_blank'
-        >
-          <p>LinkedIn</p>
-        </a>
-        <a href='https://github.com/bogmoroz' target='_blank'>
-          <p>Github</p>
-        </a>
+        <h1 className='lg-heading'>
+          Contact <span className='text-secondary'>me</span>
+        </h1>
+        <div className='boxes'>
+          <div>
+            <span className='text-secondary'> mr.bogdan.moroz@gmail.com </span>
+          </div>
+          <div>
+            <a
+              href='https://www.linkedin.com/in/bogdan-moroz-20b892113?trk=nav_responsive_tab_profile_pic'
+              target='_blank'
+            >
+              <span className='text-secondary'>LinkedIn</span>
+            </a>
+          </div>
+          <div>
+            <a href='https://github.com/bogmoroz' target='_blank'>
+              <span className='text-secondary'>GitHub</span>
+            </a>
+          </div>
+          <div>
+            <a href='https://twitter.com/bogmoroz' target='_blank'>
+              <span className='text-secondary'>Twitter</span>
+            </a>
+          </div>
+        </div>
       </div>
       <footer className='about-footer'>Bogdan Moroz &copy; 2019</footer>
     </>
