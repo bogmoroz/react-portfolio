820de314d5f2fd8bb45e5af4752400d95d5850e1
Bogdan Moroz <bogdan.moroz@observis.fi>
2019-12-23 01:45:42 +0200

    Bug fix: not showing the menu on back

--- app/components/Landing.js
+++ app/components/Landing.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import style from '../styles/main.scss';
 import Nav from './Nav';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
@@ -9,42 +9,34 @@ import Media from './Media';
 import Contacts from './Contacts';
 import MenuButton from './MenuButton';
 
-export default class Landing extends React.Component {
-  constructor(props) {
-    super(props);
+export default function Landing() {
+  const [isMenuVisible, setIsMenuVisible] = useState(false);
 
-    this.state = {
-      isMenuVisible: false
-    };
-  }
-
-  render() {
-    const { isMenuVisible } = this.state;
-    return (
-      <Router>
-        <header>
-          <MenuButton
-            handleClick={this.toggleMenu}
-            isMenuVisible={isMenuVisible}
-          />
-
-          <Nav isVisible={isMenuVisible} />
-        </header>
-        <Switch>
-          <Route exact path='/' component={Home} />
-          <Route exact path='/about' component={About} />
-          <Route exact path='/software' component={Software} />
-          <Route exact path='/media' component={Media} />
-          <Route exact path='/contacts' component={Contacts} />
-          <Route render={() => <p>Page Not Found</p>} />
-        </Switch>
-      </Router>
-    );
-  }
-
-  toggleMenu = () => {
-    this.setState(({ isMenuVisible }) => ({
-      isMenuVisible: !isMenuVisible
-    }));
+  const toggleMenu = () => {
+    setIsMenuVisible(menuVisible => !menuVisible);
   };
+
+  useEffect(() => {
+    window.onpopstate = () => {
+      setIsMenuVisible(false);
+    };
+  }, []);
+
+  return (
+    <Router>
+      <header>
+        <MenuButton handleClick={toggleMenu} isMenuVisible={isMenuVisible} />
+
+        <Nav isVisible={isMenuVisible} />
+      </header>
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route exact path='/about' component={About} />
+        <Route exact path='/software' component={Software} />
+        <Route exact path='/media' component={Media} />
+        <Route exact path='/contacts' component={Contacts} />
+        <Route render={() => <p>Page Not Found</p>} />
+      </Switch>
+    </Router>
+  );
 }
--- app/index.js
+++ app/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './components/App';
 import Landing from './components/Landing';
 
 ReactDOM.render(<Landing />, document.getElementById('app'));
