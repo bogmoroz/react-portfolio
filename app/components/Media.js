0fb7ed607d31b4f9ad549cf07d7c0ce2f763e792
Bogdan Moroz <bogdan.moroz@observis.fi>
2019-12-23 00:56:44 +0200

    Media content changes

--- app/components/Media.js
+++ app/components/Media.js
@@ -20,18 +20,18 @@ export default function Media() {
         <ProjectInfo
           title='Forget Me Not (2016)'
           description='<p>A short film I made while taking a course on visual media
-        production in Aarhus University in Denmark in the summer of 2016. My primary
-        roles during production were camera work and editing, but I have also participated
-        in all stages including scripting, storyboarding, location scouting, casting and
-        so on. The production was carried out under guidance of Dr. Richard Raskin, an
+        production at Aarhus University in Denmark in the summer of 2016. My primary
+        role during production was sound and video editing, but I participated
+        in every stage of the production including scripting, storyboarding, location scouting and 
+        camera work. The production was carried out under the guidance of Dr. Richard Raskin, an
         established short film director and theorist.</p>'
           videoUrl='https://www.youtube.com/embed/Qx4uN7dyJB4'
         ></ProjectInfo>
 
         <ProjectInfo
           title='The Last Sleepover (2015)'
-          description='<p>A short film made during Basics of Media Production course
-        in South-Eastern Finland University of Applied Sciences in the spring of 2015. This was my
+          description='<p>A short film made during the Basics of Media Production course
+        at South-Eastern Finland University of Applied Sciences in the spring of 2015. This was my
         first production where I participated in every aspect of the process. I
         wrote, storyboarded, did camera work and some acting, as well as edited the film.</p>'
           videoUrl='https://www.youtube.com/embed/-COuwI5-VBM'
@@ -40,7 +40,7 @@ export default function Media() {
         <ProjectInfo
           title='"Jukurit" Media Productions (2014 - 2015)'
           description="<p>For a year in 2014-2015 I recorded games of a Mikkeli ice-hockey
-        team 'Mikkelin Jukurit' for live broadcast and highlights reel. There I got
+        team 'Mikkelin Jukurit' for live broadcasts and highlights reels. There I got
         a lot of hands-on experience of working with a camera and setting up a large
         multi-camera production. The highlights are available on
         <a href='https://www.youtube.com/watch?v=q-k5nuuIQ6I' target='_blank'>Jukurit's YouTube channel</a>
