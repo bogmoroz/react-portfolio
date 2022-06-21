d6446be3706219cce145e33db61ece8e501bc4c8
Bogdan Moroz <mr.bogdan.moroz@gmail.com>
2022-06-21 13:14:35 +0300

    Update Software.js
    Copyright date kept up to date on Software page

--- app/components/Software.js
+++ app/components/Software.js
@@ -32,7 +32,7 @@ export default function Software() {
           ))}
         </>
       </div>
-      <footer className='about-footer'>Bogdan Moroz &copy; 2020</footer>
+      <footer className='about-footer'>`Bogdan Moroz &copy; ${new Date().getFullYear()}`</footer>
     </>
   );
 }
