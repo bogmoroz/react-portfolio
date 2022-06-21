fefa66dcaa1b6aa482f0f869cf496951a967344f
Bogdan Moroz <mr.bogdan.moroz@gmail.com>
2022-06-21 13:20:54 +0300

    Update About.js
    Date fixed in About

--- app/components/About.js
+++ app/components/About.js
@@ -87,7 +87,7 @@ export default function About() {
           </div>
         </div>
       </div>
-      <footer className='about-footer'>Bogdan Moroz &copy; 2020</footer>
+      <footer className='about-footer'>Bogdan Moroz &copy; {new Date().getFullYear()}</footer>
     </>
   );
 }
